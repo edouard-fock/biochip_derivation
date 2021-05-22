995efeec2085e530755ab3fc114efab14b4f153b
Hugo PERINAZZO <hugo.perinazzo@epitech.eu>
2021-05-22 08:35:06 +0200

    update database credentials

--- utils/api/getSql.ts
+++ utils/api/getSql.ts
@@ -4,11 +4,11 @@ const postgres = require('postgres');
 const getSql = (): any => {
   const sql = postgres('postgres://username:password@host:port/database', {
     host: 'saturn.perinazzo.com', // Postgres ip address or domain name
-    port: 45432, // Postgres server port
+    port: 45433, // Postgres server port
     path: '', // unix socket path (usually '/tmp')
-    database: 'BioChipDerivation', // Name of database to connect to
+    database: 'postgres', // Name of database to connect to
     username: 'postgres', // Username of database user
-    password: 'postgres', // Password of database user
+    password: 'eLzh78_-3#!/zxe', // Password of database user
     ssl: false, // True, or options for tls.connect
     max: 10, // Max number of connections
     timeout: 0, // Idle connection timeout in seconds
